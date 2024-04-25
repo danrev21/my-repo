99334dc8d97b0753443448e48836bdd729df278b
dan <dan.rev2021@gmail.com>
2024-04-25 22:06:18 +0300

    before new install

--- system-managing/clean-snap.sh
+++ system-managing/clean-snap.sh
@@ -2,13 +2,14 @@ sudo du -sh /var/lib/snapd
 
 
 #!/bin/bash
- #Removes old revisions of snaps
- #CLOSE ALL SNAPS BEFORE RUNNING THIS
- set -eu
- LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
-     while read snapname revision; do
-         snap remove "$snapname" --revision="$revision"
-     done
+ 
+#Removes old revisions of snaps
+#CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
      
      
      
